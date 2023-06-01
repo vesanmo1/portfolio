fde85e30ac9749c1c531130de027cb2a2c7d2e6f
jmgarcia <jmgarcia@esatvalencia.lan>
2023-06-01 19:37:27 +0200

    generic component

--- src/views/landing/Landing.jsx
+++ src/views/landing/Landing.jsx
@@ -1,7 +1,41 @@
+import { SideImageContent } from "../../components/side-image-content";
+import { SectionImageContent } from "./SectionImageContent";
 import "./landing.scss";
 
 function Landing() {
-  return <div>My landing</div>;
+  return (
+    <div className="landing">
+      <SideImageContent
+        imgAlt="Gatito tierno"
+        imgSrc="https://media.istockphoto.com/id/1345472306/es/foto/un-hermoso-gatito-de-jengibre-se-sienta-en-botes-humanos-al-atardecer-al-aire-libre-el.jpg?s=612x612&w=0&k=20&c=cFZudSbqRlHQkmbLhThMfrYau9e_s2YmRUfC2oz-3hs="
+      >
+        <button>Mi contenido</button>
+      </SideImageContent>
+      <SideImageContent
+        imgAlt="Gatito tierno"
+        alignImgRight
+        className="gatito-100"
+        contentClassName="gatito-100__content"
+        imgSrc="https://media.istockphoto.com/id/1345472306/es/foto/un-hermoso-gatito-de-jengibre-se-sienta-en-botes-humanos-al-atardecer-al-aire-libre-el.jpg?s=612x612&w=0&k=20&c=cFZudSbqRlHQkmbLhThMfrYau9e_s2YmRUfC2oz-3hs="
+      >
+        <SectionImageContent
+          title="About me"
+          description="soy murciano"
+          button="Apóyame"
+        />
+      </SideImageContent>
+      <SideImageContent
+        imgAlt="Gatito tierno"
+        imgSrc="https://media.istockphoto.com/id/1345472306/es/foto/un-hermoso-gatito-de-jengibre-se-sienta-en-botes-humanos-al-atardecer-al-aire-libre-el.jpg?s=612x612&w=0&k=20&c=cFZudSbqRlHQkmbLhThMfrYau9e_s2YmRUfC2oz-3hs="
+      >
+        <SectionImageContent
+          title="Josemi"
+          description="Tengo hambre"
+          button="Cómprame un llaollao"
+        />
+      </SideImageContent>
+    </div>
+  );
 }
 
 export default Landing;
